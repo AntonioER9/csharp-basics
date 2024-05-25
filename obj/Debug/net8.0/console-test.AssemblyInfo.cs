b0fcfebaae89589d7c33b54c1a2f3814b5365d5c
Antonio Espinoza <anespinoza@baufest.com>
2024-05-25 18:43:21 -0400

    add functional programming

--- obj/Debug/net8.0/console-test.AssemblyInfo.cs
+++ obj/Debug/net8.0/console-test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("console-test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28db6f5d08544f4c506fd393a8485a71dced8a9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68b646c3f1c8f1111a98a5010f5d77e95efa26ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("console-test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("console-test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
